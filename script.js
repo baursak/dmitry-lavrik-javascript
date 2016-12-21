62513ccc2794b954709aac6db3f7f5cf3267a18f
baursak <baursak@gmail.com>
2016-12-21 12:03:54 +0300

    02h05m18s - faq

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 window.onload = function () {
-    var images = [
-        'Chrysanthemum.jpg',
-        'Desert.jpg',
-        'Jellyfish.jpg',
-        'Penguins.jpg',
-        'Tulips.jpg'
-    ];
-    var i = 0;
-    var img = document.querySelector("#slider img");
-    img.src = "img/" + images[i];
-    document.querySelector("#btnPrev").onclick = function () {
-        (i > 1) ? i-- : (i = images.length - 1);
-        img.src = "img/" + images[i];
-    };
-    document.querySelector("#btnNext").onclick = function () {
-        (i < (images.length - 1)) ? i++ : i = 0;
-        img.src = "img/" + images[i];
-    };
+    var items = document.querySelectorAll(".item");
+    for (var i = 0; i < items.length; i++) {
+        items[i].onclick = function () {
+            var answer = this.querySelector(".answer");
+            if (answer.style.display == "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        };
+    }
 };
